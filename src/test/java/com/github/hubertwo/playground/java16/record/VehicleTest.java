3401a70dc5768393b269c7f8b936c671be54e54f
Hubert Wo <2518652+HubertWo@users.noreply.github.com>
2021-04-16 21:42:32 +0200

    Recrod - tests

--- src/test/java/com/github/hubertwo/playground/java16/record/VehicleTest.java
+++ src/test/java/com/github/hubertwo/playground/java16/record/VehicleTest.java
@@ -1,12 +1,36 @@
 package com.github.hubertwo.playground.java16.record;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class VehicleTest {
+import static com.google.common.truth.Truth.assertThat;
 
+class VehicleTest {
+    
     @Test
-    public void createRecord() {
-        final Vehicle bike = new Vehicle("Bike");
+    void createRecord() {
+        var vehicle = new Vehicle("Bike");
+
+        assertThat(vehicle.name()).isEqualTo("Bike");
     }
 
+    @Test
+    @DisplayName("Implicit inherits from Record class")
+    void recordInheritsFromRecord() {
+        var vehicle = new Vehicle("Car");
+
+        assertThat(vehicle).isInstanceOf(Record.class);
+    }
+
+    @Test
+    @DisplayName("Records might be defined in local scope")
+    void localRecord() {
+        record Fruit(String name) {
+        }
+
+        var localRecord = new Fruit("Pineapple");
+
+        assertThat(localRecord).isInstanceOf(Record.class);
+        assertThat(localRecord.name()).isEqualTo("Pineapple");
+    }
 }
