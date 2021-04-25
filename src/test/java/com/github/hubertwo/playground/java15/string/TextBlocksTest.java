fade30734a19fda04176ecc4b90842df401737c4
Hubert Wo <2518652+HubertWo@users.noreply.github.com>
2021-04-25 12:16:39 +0200

    TextBlocks - do not add line breaks test

--- src/test/java/com/github/hubertwo/playground/java15/string/TextBlocksTest.java
+++ src/test/java/com/github/hubertwo/playground/java15/string/TextBlocksTest.java
@@ -11,8 +11,8 @@ class TextBlocksTest {
      * {@see <a href="https://openjdk.java.net/jeps/378>More</a>}
      */
     @Test
-    @DisplayName("Text Blocks")
-    void textBlocks() {
+    @DisplayName("Multiline text block")
+    void multilineTextBlock() {
         String concatenatedString = "No more quote escaping \", \\n and concatenation needed!\n" +
                 "Hurray!";
 
@@ -22,4 +22,19 @@ class TextBlocksTest {
 
         assertThat(actualString).isEqualTo(concatenatedString);
     }
+
+    @Test
+    @DisplayName("Do not add line breaks to result string")
+    void doNotAddLineBreaks() {
+        String expectedString = "This is one line of text.";
+
+        // Try to remove slashes at the end of lines.
+        String actualString = """
+                This is one line\
+                 of\
+                 text.""";
+
+        assertThat(actualString).isEqualTo(expectedString);
+    }
+
 }
